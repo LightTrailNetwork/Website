2a6bcaacbfe59698702a47dd6eb83381ef24b188
LightTrailNetwork <LightTrailNetwork@gmail.com>
2025-12-06 22:05:25 -0600

    Cleanup of unused files

--- src/App-simple.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-4">
-      <h1 className="text-2xl font-bold text-red-600">Hello Mentor World - Simple Test</h1>
-      <p className="text-blue-600">If you can see this, React and Tailwind are working.</p>
-    </div>
-  );
-}
