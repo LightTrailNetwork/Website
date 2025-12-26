6684348a9468abee168b318a62512bcedc367433
LightTrailNetwork <LightTrailNetwork@gmail.com>
2025-12-26 14:22:59 -0600

    Prevent site header from reappearing when you scroll to bottom of page

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -10,8 +10,14 @@ export function useScrollDirection() {
         const updateScrollDirection = () => {
             const scrollY = window.scrollY;
             const direction = scrollY > lastScrollY ? 'down' : 'up';
+            // Ignore scroll up if we are at the bottom of the page (prevents bounce effect)
+            const isAtBottom = Math.ceil(scrollY + window.innerHeight) >= document.documentElement.scrollHeight;
+
             if (direction !== scrollDirection && (Math.abs(scrollY - lastScrollY) > 0)) {
-                setScrollDirection(direction);
+                // Only allow setting direction to 'up' if not at the bottom
+                if (direction === 'down' || !isAtBottom) {
+                    setScrollDirection(direction);
+                }
             }
             setIsAtTop(scrollY < 10);
             lastScrollY = scrollY > 0 ? scrollY : 0;
